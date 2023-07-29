a807bd5d867aba0bb3a928c0b6454c227777a8f3
Weibin Huang <654751191@qq.com>
2023-07-29 17:00:40 +0800

    v0.1.4
    + Optimize LuckyVerbose()

--- R/base_LuckyVerbose.R
+++ R/base_LuckyVerbose.R
@@ -15,7 +15,42 @@
 #' LuckyVerbose("AVC",levels = 3)
 #' LuckyVerbose("AVC",levels = 3,type="message")
 #' @export
-LuckyVerbose <- function(...,levels = 1,type = NULL){
+LuckyVerbose <- function(...,levels = 1,type = NULL, show.sys.time = T){
+  ## Verbose type
+  if(is.null(type)){
+    if(levels == 1){
+      type <-  "message"
+    } else {
+      type <-  "cat"
+    }
+  }
+
+  ## level symbol
+  if(levels > 1){
+    ls <- paste(rep("o",(levels-1)),collapse = "")
+  } else {
+    ls <- ""
+  }
+
+  ## System time
+  if(show.sys.time){
+    ls <- paste0(as.character(Sys.time()),' | ', ls)
+  }
+
+  ## do Verbose
+  if(type == "message"){
+    return(base::message(ls,...))
+  } else if(type == "cat") {
+    return(base::cat(ls,...,"\n"))
+  } else {
+    print("Please input right type.")
+  }
+
+}
+
+
+# Legacy function: 2023-07-29
+LuckyVerbose_legacy <- function(...,levels = 1,type = NULL){
   ## Verbose type
   if(is.null(type)){
     if(levels == 1){
@@ -44,4 +79,3 @@ LuckyVerbose <- function(...,levels = 1,type = NULL){
   }
 
 }
-
