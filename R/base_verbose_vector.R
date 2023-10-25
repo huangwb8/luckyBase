59a92c6ca0136e80d01b523cfa1a1e598b35b507
Weibin Huang <654751191@qq.com>
2023-10-26 07:56:02 +0800

    Enhance verbose_vector()
    + Using the table name of the vector in verbose_vector() is the default

--- R/base_verbose_vector.R
+++ R/base_verbose_vector.R
@@ -3,13 +3,16 @@
 #' @title verbose_vector
 #' @description verbose a vector. For example, report \code{c(1,2,3)} as \code{"1", "2", "3"}
 #' @param vector a vector
+#' @param tablename whether verbose table names of the vector
 #' @importFrom tidyr %>%
 #' @return a character
 #' @author Weibin Huang<\email{654751191@@qq.com}>
 #' @export
-verbose_vector <- function(vector){
+verbose_vector <- function(vector, tablename = T){
+  if(tablename){
+    vector <- names(table(vector, useNA = "always"))
+  }
   vector %>% paste0('\"',.,'\"') %>% for(i in .) cat(i,sep = "",', ')
   # return(substring(v2, 1, (nchar(v2) - 1)))
-
 }
 
