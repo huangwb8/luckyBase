c2ea2434c3151c0756bf602de3b035022be9ce24
Weibin Huang <654751191@qq.com>
2024-11-17 17:49:15 +0800

    v0.1.19
    + fix: file management of `mymusic()` function

--- R/base_mymusic.R
+++ R/base_mymusic.R
@@ -6,25 +6,18 @@
 #' @param path the path of music files ducument
 #' @return a string of music file path
 #' @author Weibin Huang<\email{654751191@@qq.com}>
-#' @importFrom tuneR play
+#' @import tuneR
 #' @examples
 #' mymusic(1)
 #' @export
 mymusic <- function(n=1, path=NULL){
   if(is.null(path)){
-    path <- "E:/RCloud/music"
+    # path <- "E:/RCloud/music"
+    path <- system.file("extdata", package = "luckyBase")
   }
-  music.list <- list.files(path,pattern = "Rend_",full.names = T)
-  if(length(music.list)==0){
-    # 在给定的地址中未找到音频文件。
-    library(tuneR)
-    w <- system.file("extdata", "wind 00_00_04-00_00_28.m4a", package = "lucky")
-    tuneR::play(w)
-  } else {
-    # 可以找到文件
-    music <- music.list[n];
-    tuneR::play(music)
-  }
+  music.list <- list.files(path, pattern = "Rend_",full.names = T)
+  music <- music.list[n]
+  tuneR::play(music)
 }
 
 
